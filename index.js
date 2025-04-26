b121600a2af5df47774dac952fc448d4c6c1da3b
ANSHUL GOLA <anshulgola5339@gmail.com>
2025-04-26 23:13:09 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+var Path = `M 10 100 Q 500 100 990 100`
+
+var finalPath = `M 10 100 Q 500 100 990 100`
+
+
+ 
+var string = document.querySelector("#string")
+
+string.addEventListener(
+    "mousemove",
+    function(dets)
+    {
+      Path = `M 10 100 Q ${dets.x} ${dets.y} 990 100`,
+      gsap.to("svg path", {
+        attr:{ d: Path },
+        duration:0.2,
+        ease: "power3.out",  
+      });
+    }
+)
+
+
+string.addEventListener(
+    "mouseleave",
+    function()
+    {
+        gsap.to("svg path",{
+          attr:{d:finalPath},
+          duration:2,
+          ease:"elastic.out(1,0.2)"
+        })
+    }
+)
